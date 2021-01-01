51558003b1b6239231a36d22d6a1f1917d3fde86
Paul Sammut <paul@pop-os.localdomain>
2021-01-01 20:44:46 +1100

    .

--- client/app.svelte
+++ client/app.svelte
@@ -1,40 +1,11 @@
 <script>
-    export let pid = 'vlctv'
-	async function fetchProject(pid) {
-        const res = await fetch(`api/projects/${pid}`);
-        const json = await res.json()
-        return json;
-    }
-    async function fetchApps() {
-        const res = await fetch(`api/apps`);
-        const json = await res.json()
-        const apps = json.map(([k, v]) => k)
-        return apps;
-    }
-    function launchApp(appId) {
-        fetch(`api/launch/${appId}/${pid}`);
-    }
+    import Project from './project.svelte'
+    import router from 'page'
+    let page
+    // router('/', () => page = Home)
+    router('/about', () => page = Project)
 
-    fetchProject(pid).then((res) => console.log(res))
+    router.start() 
 </script>
-
+<svelte:component this={page} />
 <h1>THIS IS THE APP</h1>
-<main>
-    {#await fetchApps() then apps}
-        {#each apps as aid, i}
-            <button on:click={() => launchApp(aid)}><img src="{aid}.png" alt="Submit"></button>
-        {/each}
-    {/await}
-</main>
-
-<style>
-    main {
-        display:flex;
-        justify-content: center;
-        gap:32px;
-    }
-    img {
-        height: 64px;
-        width: 64px;
-    }
-</style>
