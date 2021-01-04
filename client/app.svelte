f7642b7eaa1fa3edd713d9d68390ff309f55d481
Paul Sammut <paul@pop-os.localdomain>
2021-01-04 16:51:26 +1100

    removed pid, url, basepath

--- client/app.svelte
+++ client/app.svelte
@@ -1,10 +1,8 @@
 <script>
     import { Router, Route } from 'svelte-routing'
     import Project from './project.svelte'
-    let pid
-    export let url = ''
 </script>
-<Router {url} basepath='/'>
+<Router>
     <Route path='project/:pid' let:params>
         <Project pid={params.pid}/>
     </Route>
