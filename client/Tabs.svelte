3399e10ef5ce6dcdea60a7e1f54df302d98d0e06
Paul Sammut <paul@pop-os.localdomain>
2021-01-08 18:26:08 +1100

    .

--- client/Tabs.svelte
+++ client/Tabs.svelte
@@ -10,7 +10,7 @@
 <style>
     .box {
         margin-bottom: 10px;
-        padding: 40px;
+        /* padding: 40px; */
         border: 1px solid #dee2e6;
         border-radius: 0 0 0.5rem 0.5rem;
         border-top: 0;
--- client/markdown.svelte
+++ client/markdown.svelte
@@ -1,5 +1,27 @@
 <script>
     import marked from 'marked';
     export let content = "# Default Content"
+    export let editMode = false
 </script>
-{@html marked(content)}
+{#if editMode}
+    <textarea>
+        {content}
+    </textarea>
+{:else}
+    <div>
+        {@html marked(content)}
+    </div>
+{/if}
+<style>
+    textarea {
+        height: 100%;
+        width: 100%;
+        border: none;
+        margin: 0;
+    }
+    div {
+        padding-left: 40px;
+        padding-right: 40px;
+    }
+</style>
+
